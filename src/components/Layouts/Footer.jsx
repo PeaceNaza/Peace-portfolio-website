1ff2464572bba3ba445b2129c62a8921181d6508
PeaceNaza <peacedonatus38@gmail.com>
2024-05-11 08:15:35 +0100

    feat: add social media icons to footer

--- src/components/Layouts/Footer.jsx
+++ src/components/Layouts/Footer.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import desk from "../../assets/footer_mobile_bkg.jpg"
 import "../Styles/App.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
 
 
 const Footer = () => {
@@ -13,6 +17,13 @@ const Footer = () => {
           <p className="text-2xl font-extralight">
           Quality is never an accident; it is always the result of intelligent effort.
           </p>
+          <div className="flex justify-center gap-7">
+          <a href="https://github.com/PeaceNaza"><FontAwesomeIcon icon={faGithub} className="text-4xl text-[#22024A] mt-10" /></a>
+          <a href="https://www.linkedin.com/in/peace-nwosu-69047623a/"><FontAwesomeIcon icon={faLinkedin} className="text-4xl text-[#22024A] mt-10" /></a>
+          <a href="https://twitter.com/PeaceNwosu11"><FontAwesomeIcon icon={ faTwitter} className="text-4xl text-[#22024A] mt-10" /></a>
+
+          </div>
+          
           </div>
           <div>
             <img src={desk} alt="office pink table and desk" />
