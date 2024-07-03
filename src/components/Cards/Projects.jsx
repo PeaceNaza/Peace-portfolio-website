71abb2851c12caeaa78fa0f8889b1b68f553b2fd
PeaceNaza <peacedonatus38@gmail.com>
2024-07-03 07:26:07 +0100

    refactor: Update Projects component layout and styles

--- src/components/Cards/Projects.jsx
+++ src/components/Cards/Projects.jsx
@@ -5,176 +5,99 @@ import { faEye } from '@fortawesome/free-regular-svg-icons'
 import "../Styles/App.css"
 
 const Projects = () => {
+  const projectDetails = [
+    {
+      title: "Tmdb api Movie App",
+      stack: "REACT + TailwindCSS +  useQuery",
+      description: "Movie data from tmdb api for web application",
+      codeLink: "https://github.com/PeaceNaza/movie-mate-tmdb.git",
+      liveLink: "https://movie-mate-tmdb.vercel.app/",
+    },
+    {
+      title: "News Homepage",
+      stack: "REACT + TailwindCSS +  Mantine",
+      description: "News Homepage challenge from Frontend Mentor",
+      codeLink: "https://github.com/PeaceNaza/news_homepage_fm",
+      liveLink: "https://news-homepage-fm-three.vercel.app/",
+    }, 
+    {
+      title: "Feedback Review App",
+      stack: "REACT + CSS + LOCALSTORAGE",
+      description: "Feedback Review App with user ratings and comments",
+      codeLink: "https://github.com/PeaceNaza/Feedback-App.git",
+      liveLink: "https://feedback-app-beige-mu.vercel.app/",
+    }, 
+    {
+      title: "Task Manager",
+      stack: "REACT + TailwindCSS + Zustand",
+      description: "State management with Zustand",
+      codeLink: "https://github.com/PeaceNaza/Zustand-mgt-store.git",
+      liveLink: "https://zustand-mgt-store.vercel.app/",
+    },
+    {
+      title: "Todo App",
+      stack: "React + TailwindCSS + LOCALSTORAGE",
+      description: "Todo App for efficient task management",
+      codeLink: "https://github.com/PeaceNaza/Todo-app.git",
+      liveLink: "https://todo-app-zeta-kohl.vercel.app/",
+    }, 
+    {
+      title: "Quiz App",
+      stack: "HTML + CSS + javaScript",
+      description: "Random question with multiple choices",
+      codeLink: "https://github.com/PeaceNaza/Quiz-App.git",
+      liveLink: "https://quiz-app-taupe-eight.vercel.app/",
+    }, 
+    {
+      title: "FAQ Dropdown Accordion",
+      stack: "HTML + CSS + JavaScript",
+      description: "FAQ Dropdown Accordion for users to access frequently asked questions",
+      codeLink: "https://github.com/PeaceNaza/Faq-accordion-challenge.git",
+      liveLink: "https://faq-accordion-challenge-tau.vercel.app/",
+    },
+    {
+      title: "Login and Sign-Up Page",
+      stack: "React + TailwindCSS + Firebase",
+      description: "Login and Sign-Up with secure Firebase authentication",
+      codeLink: "https://github.com/PeaceNaza/Firebase-Auth.git",
+      liveLink: "https://firebase-project-tan.vercel.app/",
+    }
+  ]
   
   return (
     <>
     <section className="my-10 pb-10 px-3">
     <h1 className="text-center mt-8 font-bold text-2xl text-[#22024A]">Project Highlights</h1>
-   
-    <div className="mt-5 grid lg:grid-cols-3 sm:grid-cols-2 px-2 gap-y-5">
+    <div className="flex flex-wrap justify-start gap-x-5 gap-y-5 mt-5 md:ml-10">
+    {projectDetails.map((project, index) =>  (
+      <div key={index}>
+      <div className="card1 ml-2 w-[360px] h-[200px]  text-white bg-[#22024A] shadow-xl transform transition-transform duration-300 hover:scale-[1.08] text-start text-wrap">
+        <div className="card-body">
+          
+            <h2 className="card-title font-extralight text-xl">
+              {project.title}
+            </h2>
+            <p className="text-[10.4px] font-medium leading-8">{project.stack}</p>
+            <p className="font-extralight text-sm">{project.description}</p>
+            <div className="card-actions justify-start text-sm font-extralight gap-5 leading-8">
+              <div className="flex justify-between text-sm font-extralight leading-8">
+                <a href={project.codeLink}> <FontAwesomeIcon icon={faGithub} className="mr-2" />
+                  Code</a>
+              </div>
+              <div className="flex justify-between">
+                <a href={project.liveLink}> <FontAwesomeIcon icon={faEye} className="mr-2" />
+                  Live</a>
+              </div>
+            </div>
+       
+     </div>
+    </div>
+    </div>
+    ))}
 
-      <div className="card1 ml-2 text-white w-[360px] sm:h-[200px] bg-[#22024A] shadow-xl transform transition-transform duration-300 hover:scale-[1.08] text-start text-wrap">
-       <div className="card-body">
-        <h2 className="card-title font-extralight text-xl">
-        Tmdb api Movie App
-     </h2>
-      <p className="text-[10.4px] font-medium leading-8">REACT + TailwindCSS +  useQuery</p>
-      <p className="font-extralight text-sm">Movie data from tmdb api for web application </p>
-       <div className="card-actions justify-start text-sm font-extralight gap-5 leading-8">
-       <div className="flex justify-between text-sm font-extralight leading-8">
-           <a href="https://github.com/PeaceNaza/movie-mate-tmdb.git"> <FontAwesomeIcon icon={faGithub} className="mr-2" />
-           Code</a>
-         </div>
-       <div className="flex justify-between">
-         <a href="https://movie-mate-tmdb.vercel.app/"> <FontAwesomeIcon icon={faEye} className="mr-2" />
-           Live</a>
-         </div>
-       </div>
-    </div>
-</div>
 
-      <div className="card1 ml-2 text-white w-[360px] sm:h-[200px] bg-[#22024A] shadow-xl transform transition-transform duration-300 hover:scale-[1.08] text-start text-wrap">
-       <div className="card-body">
-        <h2 className="card-title font-extralight text-xl">
-        News Homepage
-     </h2>
-      <p className="text-[10.4px] font-medium leading-8">REACT + TailwindCSS +  Mantine</p>
-      <p className="font-extralight text-sm">News Homepage challenge from Frontend Mentor </p>
-       <div className="card-actions justify-start text-sm font-extralight gap-5 leading-8">
-       <div className="flex justify-between text-sm font-extralight leading-8">
-           <a href="https://github.com/PeaceNaza/news_homepage_fm"> <FontAwesomeIcon icon={faGithub} className="mr-2" />
-           Code</a>
-         </div>
-       <div className="flex justify-between">
-         <a href="https://news-homepage-fm-three.vercel.app/"> <FontAwesomeIcon icon={faEye} className="mr-2" />
-           Live</a>
-         </div>
-       </div>
-    </div>
-</div>
-      
-      <div className="card1 ml-2 text-white w-[360px] sm:h-[200px] bg-[#22024A] shadow-xl transform transition-transform duration-300 hover:scale-[1.08] text-start text-wrap">
-       <div className="card-body">
-        <h2 className="card-title font-extralight text-xl">
-        Feedback Review App
-     </h2>
-      <p className="text-[10.4px] font-medium leading-8">REACT + CSS + LOCALSTORAGE</p>
-      <p className="font-extralight text-sm">Feedback Review App with user ratings and comments</p>
-       <div className="card-actions justify-start text-sm font-extralight gap-5 leading-8">
-       <div className="flex justify-between text-sm font-extralight leading-8">
-           <a href="https://github.com/PeaceNaza/Feedback-App.git"> <FontAwesomeIcon icon={faGithub} className="mr-2" />
-           Code</a>
-         </div>
-       <div className="flex justify-between">
-         <a href="https://feedback-app-beige-mu.vercel.app/"> <FontAwesomeIcon icon={faEye} className="mr-2" />
-           Live</a>
-         </div>
-       </div>
-    </div>
-</div>
     
-      <div className="card1 ml-2 text-white w-[360px] sm:h-[200px] bg-[#22024A] shadow-xl transform transition-transform duration-300 hover:scale-[1.08] text-start text-wrap">
-       <div className="card-body">
-        <h2 className="card-title font-extralight text-xl">
-        Task Manager
-     </h2>
-      <p className="text-[10.4px] font-medium leading-8">REACT + TailwindCSS + Zustand</p>
-      <p className="font-extralight text-sm">State management with Zustand</p>
-       <div className="card-actions justify-start text-sm font-extralight gap-5 leading-8">
-       <div className="flex justify-between text-sm font-extralight leading-8">
-           <a href="https://github.com/PeaceNaza/Zustand-mgt-store.git"> <FontAwesomeIcon icon={faGithub} className="mr-2" />
-           Code</a>
-         </div>
-       <div className="flex justify-between">
-         <a href="https://zustand-mgt-store.vercel.app/"> <FontAwesomeIcon icon={faEye} className="mr-2" />
-           Live</a>
-         </div>
-       </div>
     </div>
-</div>
-      
-      <div className="card1 ml-3 text-white w-[360px] sm:h-[200px] bg-[#22024A] shadow-xl transform transition-transform duration-300 hover:scale-[1.08]">
-       <div className="card-body">
-        <h2 className="card-title font-extralight text-xl">
-        Todo App
-     </h2>
-     <p className="text-[10.4px] font-medium leading-8">React + TailwindCSS + LOCALSTORAGE</p>
-      <p  className="font-extralight text-sm">Todo App for efficient task management</p>
-       <div className="card-actions justify-start text-sm font-extralight gap-5 leading-8">
-       <div className="flex justify-between text-sm font-extralight leading-8">
-           <a href="https://github.com/PeaceNaza/Todo-app.git"> <FontAwesomeIcon icon={faGithub} className="mr-2" />
-           Code</a>
-         </div>
-       <div className="flex justify-between">
-         <a href="https://todo-app-zeta-kohl.vercel.app/"> <FontAwesomeIcon icon={faEye} className="mr-2" />
-           Live</a>
-         </div>
-       </div>
-    </div>
-</div>
-     
-      <div className="card1 ml-2 text-white w-[360px] sm:h-[200px] bg-[#22024A] shadow-xl transform transition-transform duration-300 hover:scale-[1.08]">
-       <div className="card-body">
-        <h2 className="card-title font-extralight text-xl">
-        Quiz App
-     </h2>
-      <p className="text-[10.4px] font-medium leading-8">HTML + CSS + javaScript</p>
-      <p  className="font-extralight text-sm">Random question with multiple choices</p>
-       <div className="card-actions justify-start text-sm font-extralight gap-5 leading-8">
-         <div className="flex justify-between text-sm font-extralight leading-8">
-           <a href="https://github.com/PeaceNaza/Quiz-App.git"> <FontAwesomeIcon icon={faGithub} className="mr-2" />
-           Code</a>
-         </div>
-         <div className="flex justify-between">
-         <a href="https://feedback-app-beige-mu.vercel.app/"> <FontAwesomeIcon icon={faEye} className="mr-2" />
-           Live</a>
-         </div>
-       </div>
-    </div>
-</div>
-      
-      <div className="card1 ml-1 text-white w-[360px] sm:h-[200px] bg-[#22024A] shadow-xl transform transition-transform duration-300 hover:scale-[1.08]">
-       <div className="card-body">
-        <h2 className="card-title font-extralight text-xl">
-        FAQ Dropdown Accordion
-     </h2>
-     <p className="text-[10.4px] font-medium leading-8">HTML + CSS + JavaScript</p>
-      <p  className="font-extralight text-sm"> FAQ Dropdown Accordion for users to access frequently asked questions</p>
-       <div className="card-actions justify-start text-sm font-extralight gap-5 leading-8">
-         <div className="flex justify-between text-sm font-extralight leading-8">
-           <a href="https://github.com/PeaceNaza/Faq-accordion-challenge.git"> <FontAwesomeIcon icon={faGithub} className="mr-2" />
-           Code</a>
-         </div>
-         <div className="flex justify-between">
-         <a href="https://faq-accordion-challenge-tau.vercel.app/"> <FontAwesomeIcon icon={faEye} className="mr-2" />
-           Live</a>
-         </div>
-       </div>
-    </div>
-  </div>
-     
-      <div className="card1 text-white w-[360px] sm:h-[200px] bg-[#22024A] shadow-xl transform transition-transform duration-300 hover:scale-[1.08] ml-3">
-       <div className="card-body">
-        <h2 className="card-title font-extralight text-xl">
-        Login and Sign-Up Page
-     </h2>
-      <p className="text-[10.4px] font-medium leading-8">React + TailwindCSS + Firebase</p>
-      <p  className="font-extralight text-sm">Login and Sign-Up with secure Firebase authentication</p>
-       <div className="card-actions justify-start text-sm font-extralight gap-5 leading-8">
-         <div className="flex justify-between text-sm font-extralight leading-8">
-           <a href="https://github.com/PeaceNaza/Firebase-Auth.git"> <FontAwesomeIcon icon={faGithub} className="mr-2" />
-           Code</a>
-         </div>
-         <div className="flex justify-between">
-         <a href="https://firebase-project-tan.vercel.app/"> <FontAwesomeIcon icon={faEye} className="mr-2" />
-           Live</a>
-         </div>
-       </div>
-    </div>
-</div>
-     
-    
-   </div>
  </section>
 </>
   )
